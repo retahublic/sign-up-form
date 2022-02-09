965a68d31a8d64fc9a89367a82ada210244ddf52
retahublic <81122053+retahublic@users.noreply.github.com>
2022-02-09 10:29:14 -0500

    Update populate.js

--- populate.js
+++ populate.js
@@ -1,15 +1,1 @@
-fetch('https://frontend-take-home.fetchrewards.com/form')
-  .then(res => 
-    {
-    if (res.ok) 
-    {
-      console.log('SUCCESS')
-    }
-    else
-    {
-      console.log("Not Success")
-    }
-  }
-      res.json())
-  .then(data => console.log())
-  .catch(error => console.log ('ERROR'))
+//populate info from link to JSON to add to drop downs
